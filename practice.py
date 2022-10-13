b7681e341d0ca56968dce59f456f86e45290ab6a
seiya <butabaradaikon22@gmail.com>
2022-10-13 18:45:21 +0900

    first commit

---
+++ practice.py
@@ -0,0 +1,31 @@
+class Item:
+
+    def __init__(self, name, price):
+        self.__name = name
+        self.__price = price
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        self.__price = max(value, 0)
+
+
+x = Item("burger", 100)
+print(x.name, x.price)
+
+x.price = 150
+print(x.name, x.price)
+
+x.price = -100
+print(x.name, x.price)
+
+a = 0
+if a:
+    print("F")
